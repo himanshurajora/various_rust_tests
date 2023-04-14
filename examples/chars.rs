6681fb3bbdb9e5b456ab8432b10fdad2340fc61e
Himanshu Jangid <iamahalofan@gmail.com>
2023-04-15 02:21:51 +0530

    fix: ownership issue and complete program

--- examples/chars.rs
+++ examples/chars.rs
@@ -3,14 +3,17 @@ use std::collections::HashMap;
 fn main() {
     let input = String::from("aaaabbbbcccddee");
 
-    let mut map: HashMap<&char, u16> = HashMap::new();
+    let mut map: HashMap<char, u16> = HashMap::new();
 
-    for i in input.chars() {
-        let index = i;
-        let index2 = i;
-        if let Some(value) = map.get(&index) {
-            map.insert(&index2, *value + 1);
-            continue;
+    for ch in input.chars() {
+        println!("{}", &ch);
+        let count = map.get(&ch);
+
+        if let Some(c) = count {
+            map.insert(ch.to_owned(), c + 1);
+        } else {
+            map.insert(ch.to_owned(), 1);
         }
     }
+    println!("{:?}", map);
 }
